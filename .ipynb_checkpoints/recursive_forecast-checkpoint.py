7fd3625ea89dc454b45017cfc11113b78b99e0b0
Huan Le - Intern <Huan.Le@bentley.com>
2022-05-13 08:06:14 -0500

    Commmit #2

--- .ipynb_checkpoints/recursive_forecast-checkpoint.py
+++
@@ -1,9 +0,0 @@
-"""
-    Recursive forecast
-    Parameters:
-        n: number of preceding points to look back
-    Formula;
-        y_n = f(y_0,...,y_n)
-            = f(y)          y is shorthanded notation for column vector {y_0, y_1, ..., y_n}.T
-            ≈ f(0) + y*(grad_f(0)) + 0.5 * y.T * Hessian_f(0) * y        (2nd-order Taylor approximation)
-"""
